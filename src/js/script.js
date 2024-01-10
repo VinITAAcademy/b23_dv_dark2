4f987f6e1dd45bdf03594fa0f6a540ac760629d2
Roman <fluiq4416www@gmail.com>
2024-01-10 14:51:04 +0200

    add block_6

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,15 @@ imgclose.onclick = function(){
     youtubeOpen.style.display='block';
 }
 //"we provide video end"
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+        navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
